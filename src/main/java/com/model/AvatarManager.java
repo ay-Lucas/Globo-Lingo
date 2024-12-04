19f02af9e2880f082661fd38fb6ecc28e3730c88
Lucas <developer.lucas@pm.me>
2024-12-03 22:09:42 -0500

    fix AvatarManager paths to use pngs and refactor old loadDefaultAvatars and loadCrateAvatars

--- src/main/java/com/model/AvatarManager.java
+++ src/main/java/com/model/AvatarManager.java
@@ -1,6 +1,7 @@
 package com.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -12,9 +13,75 @@ import java.util.List;
 public class AvatarManager {
 
     private List<Avatar> avatars = new ArrayList<>();
-    private final String RESOURCES_PATH = "./src/main/resources";
-    private final String DEFAULT_AVATAR_DIR = "/default_avatar/";
-    private final String LOOT_CRATE_AVATAR_DIR = "/loot_crate_avatar/";
+    private final String RESOURCES_PATH = "/images";
+    private final String DEFAULT_AVATAR_DIR = "/default2.0/";
+    private final String LOOT_CRATE_AVATAR_DIR = "/Loot_Crate2.0/";
+    private final String FILE_ENDING = ".png";
+    private final List<String> DEFAULT_AVATAR_NAMES = List.of(
+            "Pencil Warrior",
+            "Potato Rider",
+            "Basic Bobby",
+            "Generic Guy",
+            "Black Ninja",
+            "Tired Tiger",
+            "Unremarkable Ray",
+            "Chill Crusader",
+            "Bland Bandit",
+            "Unfinished Quest",
+            "Paper Sword",
+            "Monkey Fartking");
+    private final List<String> CRATE_AVATAR_NAMES = List.of(
+            "Keyboard Warrior",
+            "Shadow Wing",
+            "Fire Storm",
+            "Star Hunter",
+            "Iron Fang",
+            "Night Blade",
+            "Frost Wolf",
+            "Dark Flame",
+            "Sky Rider",
+            "Stone Fist",
+            "Red Eagle",
+            "Storm Runner",
+            "Iron Claw",
+            "Moon Shade",
+            "Thunder Strike",
+            "Blaze Wing",
+            "Blue Hawk",
+            "Earth Guard",
+            "Wind Chaser",
+            "Flame Heart",
+            "Night Warden",
+            "Ice Claw",
+            "Steel Horn",
+            "Solar Hawk",
+            "Dark Warden",
+            "Frost Blade",
+            "Wind Shadow",
+            "Rock Fang",
+            "Fire Fist",
+            "Night Flare",
+            "Sky Blade",
+            "Steel Wolf",
+            "Iron Wing",
+            "Ice Hunter",
+            "Thunder Claw",
+            "Dark Blade",
+            "Stone Guard",
+            "Fire Hawk",
+            "Red Wolf",
+            "Shadow Strike",
+            "Storm Wing",
+            "Sky Flame",
+            "Blue Faang",
+            "Night Rider",
+            "Wind Blade",
+            "Rock Storm",
+            "Flame Claw",
+            "Earth Wing",
+            "Ice Shade",
+            "Thunder Wing",
+            "Dark Storm");
 
     /**
      * Creates a new AvatarManager
@@ -29,75 +96,17 @@ public class AvatarManager {
      * Adds default avatars to avatars ArrayList
      */
     private void loadDefaultAvatars() {
-        avatars.add(new Avatar("Pencil Warrior", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Pencil Warrior", true));
-        avatars.add(new Avatar("Potato Rider", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Potato Rider", true));
-        avatars.add(new Avatar("Basic Bobby", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Basic Bobby", true));
-        avatars.add(new Avatar("Generic Guy", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Generic Guy", true));
-        avatars.add(new Avatar("Black Ninja", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Black Ninja", true));
-        avatars.add(new Avatar("Tired Tiger", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Tired Tiger", true));
-        avatars.add(new Avatar("Unremarkable Ray", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Unremarkable Ray", true));
-        avatars.add(new Avatar("Chill Crusader", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Chill Crusader", true));
-        avatars.add(new Avatar("Bland Bandit", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Bland Bandit", true));
-        avatars.add(new Avatar("Unfinished Quest", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Unfinished Quest", true));
-        avatars.add(new Avatar("Paper Sword", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Paper Sword", true));
-        avatars.add(new Avatar("Monkey Fartking", RESOURCES_PATH + DEFAULT_AVATAR_DIR + "Monkey Fartking", true));
+        for (String e : DEFAULT_AVATAR_NAMES)
+            avatars.add(new Avatar(e, RESOURCES_PATH + DEFAULT_AVATAR_DIR + e + FILE_ENDING, true));
+
     }
 
     /**
      * Adds loot crate avatars to avatars ArrayList
      */
     private void loadCrateAvatars() {
-        avatars.add(new Avatar("Keyboard Warrior", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Keyboard Warrior", false));
-        avatars.add(new Avatar("Shadow Wing", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Shadow Wing", false));
-        avatars.add(new Avatar("Fire Storm", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Fire Storm", false));
-        avatars.add(new Avatar("Star Hunter", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Star Hunter", false));
-        avatars.add(new Avatar("Iron Fang", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Iron Fang", false));
-        avatars.add(new Avatar("Night Blade", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Night Blade", false));
-        avatars.add(new Avatar("Frost Wolf", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Frost Wolf", false));
-        avatars.add(new Avatar("Dark Flame", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Dark Flame", false));
-        avatars.add(new Avatar("Sky Rider", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Sky Rider", false));
-        avatars.add(new Avatar("Stone Fist", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Stone Fist", false));
-        avatars.add(new Avatar("Red Eagle", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Red Eagle", false));
-        avatars.add(new Avatar("Storm Runner", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Storm Runner", false));
-        avatars.add(new Avatar("Iron Claw", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Iron Claw", false));
-        avatars.add(new Avatar("Moon Shade", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Moon Shade", false));
-        avatars.add(new Avatar("Thunder Strike", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Thunder Strike", false));
-        avatars.add(new Avatar("Blaze Wing", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Blaze Wing", false));
-        avatars.add(new Avatar("Blue Hawk", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Blue Hawk", false));
-        avatars.add(new Avatar("Earth Guard", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Earth Guard", false));
-        avatars.add(new Avatar("Wind Chaser", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Wind Chaser", false));
-        avatars.add(new Avatar("Flame Heart", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Flame Heart", false));
-        avatars.add(new Avatar("Night Warden", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Night Warden", false));
-        avatars.add(new Avatar("Ice Claw", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Ice Claw", false));
-        avatars.add(new Avatar("Steel Horn", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Steel Horn", false));
-        avatars.add(new Avatar("Solar Hawk", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Solar Hawk", false));
-        avatars.add(new Avatar("Dark Warden", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Dark Warden", false));
-        avatars.add(new Avatar("Frost Blade", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Frost Blade", false));
-        avatars.add(new Avatar("Wind Shadow", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Wind Shadow", false));
-        avatars.add(new Avatar("Rock Fang", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Rock Fang", false));
-        avatars.add(new Avatar("Fire Fist", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Fire Fist", false));
-        avatars.add(new Avatar("Night Flare", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Night Flare", false));
-        avatars.add(new Avatar("Sky Blade", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Sky Blade", false));
-        avatars.add(new Avatar("Steel Wolf", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Steel Wolf", false));
-        avatars.add(new Avatar("Iron Wing", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Iron Wing", false));
-        avatars.add(new Avatar("Ice Hunter", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Ice Hunter", false));
-        avatars.add(new Avatar("Thunder Claw", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Thunder Claw", false));
-        avatars.add(new Avatar("Dark Blade", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Dark Blade", false));
-        avatars.add(new Avatar("Stone Guard", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Stone Guard", false));
-        avatars.add(new Avatar("Fire Hawk", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Fire Hawk", false));
-        avatars.add(new Avatar("Red Wolf", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Red Wolf", false));
-        avatars.add(new Avatar("Shadow Strike", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Shadow Strike", false));
-        avatars.add(new Avatar("Storm Wing", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Storm Wing", false));
-        avatars.add(new Avatar("Sky Flame", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Sky Flame", false));
-        avatars.add(new Avatar("Blue Faang", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Blue Faang", false));
-        avatars.add(new Avatar("Night Rider", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Night Rider", false));
-        avatars.add(new Avatar("Wind Blade", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Wind Blade", false));
-        avatars.add(new Avatar("Rock Storm", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Rock Storm", false));
-        avatars.add(new Avatar("Flame Claw", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Flame Claw", false));
-        avatars.add(new Avatar("Earth Wing", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Earth Wing", false));
-        avatars.add(new Avatar("Ice Shade", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Ice Shade", false));
-        avatars.add(new Avatar("Thunder Wing", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Thunder Wing", false));
-        avatars.add(new Avatar("Dark Storm", RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + "Dark Storm", false));
+        for (String e : CRATE_AVATAR_NAMES)
+            avatars.add(new Avatar(e, RESOURCES_PATH + LOOT_CRATE_AVATAR_DIR + e + FILE_ENDING, false));
     }
 
     /**
